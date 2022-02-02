cf01eef3a4f94c08f1e627426022701c4fc29dc2
afk2777 <afk2777@gmail.com>
2022-02-02 22:18:58 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -u
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+
+    # シンボリックリンクを貼る
+    ln -snfv ${PWD}/"$f" ~/
+done
+
